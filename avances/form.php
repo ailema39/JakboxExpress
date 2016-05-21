38fec560c83976fe100f5ce6387b8999b992b79b
Yomar Medina <ailema39@gmail.com>
2016-05-21 14:15:01 -0500

    Cambio de estructura, avances generales y pagina de contacto

---
+++ avances/form.php
@@ -0,0 +1,49 @@
+<?php
+
+	$emailTo = "info@jakboxexpress.com";//change this to the email address which should receive the form data
+    $emailFrom = "<contacto@jakboxexpress.com>";
+    $headers = "From: Formulario de Contacto " . $emailFrom . "\r\n";
+
+	/*
+		NOTE: do not change anything below this
+	*/
+
+	if( isset($_POST['maximus']) ) {
+
+		//honey pot detection
+
+		if( $_POST['maximus'] != '' ) {
+
+			die('Bad spam bot!!');
+
+		}
+
+		$message = "";
+
+		foreach( $_POST as $field => $val ) {
+
+			if( $field != 'maximus' && $field != 'g-recaptcha-response' && $field != 'theSubject') {
+
+				$message .= $field . ": ". $val . "\n";
+
+			}
+
+		}
+
+		$subject = $_POST['theSubject'];
+
+		//send the email
+
+		if( mail($emailTo, $subject, $message, $headers) ) {
+
+			echo "Message sent!";
+
+		} else {
+
+			echo "Could not send message";
+
+		}
+
+	}
+
+?>
